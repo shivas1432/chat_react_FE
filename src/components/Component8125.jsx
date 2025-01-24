88f7b9f0d1a5e9a41b07770e97722e722223870c
shivas1432 <shivashanker7337@gmail.com>
2025-01-24 13:02:39 +0000

    Add dark mode support

---
+++ src/components/Component8125.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+const Component8125 = () => {
+  return (
+    <div className="component-8125">
+      <h2>Component 8125</h2>
+      <p>This is component number 8125</p>
+    </div>
+  );
+};
+
+export default Component8125;
